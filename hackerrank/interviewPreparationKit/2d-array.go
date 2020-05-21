1ef506fc1760bad711d801a08eb8c54579aa958f
Samuel Loza Ramirez <starsaminf@gmail.com>
2020-05-20 20:58:31 -0400

    My solutions hackerrank

---
+++ hackerrank/interviewPreparationKit/2d-array.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the hourglassSum function below.
+func hourglassSum(arr [][]int32) int32 {
+
+    max := int32(-9*7)
+
+    for i:=0; i < len(arr); i++ {
+        if i + 2 < len(arr){
+            for j :=0; j < len(arr); j++{
+                if j + 2 < len(arr) {
+                    tmp := arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                    tmp += arr[i+1][j+1]
+                    tmp += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+                    
+                    if tmp > max{
+                        max = tmp
+                    }
+                }
+            }
+        }
+    }
+    
+    return max
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    var arr [][]int32
+    for i := 0; i < 6; i++ {
+        arrRowTemp := strings.Split(readLine(reader), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != int(6) {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+
+    result := hourglassSum(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
